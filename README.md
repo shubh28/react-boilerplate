8898ac732094abbfe70d74d9d64907f05a3ade84
NAMAN <naman@localhost.localdomain>
2016-11-12 23:49:30 +0530

    Final nail in the coffin

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-boilerplate
 A react starter project with babel and webpack configured.
+
+To run the project run:
+1. npm run dev.
+2. npm start.
--- src/app/index.js
+++ src/app/index.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render () {
     return (
         <div>
-          <p>Heoolo!</p>
+          <p>heya!</p>
           <Test/>
         </div>
       );
